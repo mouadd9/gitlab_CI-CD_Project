3c8ecfea65c51d9d6f2372c861d1e5f1e2676341
mouadispunk <bencaidmouad9@gmail.com>
2025-07-16 06:47:42 +0100

    CI pipeline structure

---
+++ src/test/java/org/cicd/accountservice/AbstractIntegrationTest.java
@@ -0,0 +1,29 @@
+package org.cicd.accountservice;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create");
+    }
+}
+
--- src/test/java/org/cicd/accountservice/controller/BankAccountControllerIT.java
+++ src/test/java/org/cicd/accountservice/controller/BankAccountControllerIT.java
@@ -1,26 +1,21 @@
 package org.cicd.accountservice.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.cicd.accountservice.AbstractIntegrationTest;
 import org.cicd.accountservice.dto.BankAccountDTO;
 import org.cicd.accountservice.service.BankAccountService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
-@AutoConfigureMockMvc
-@Transactional
-class BankAccountControllerTest {
+class BankAccountControllerIT extends AbstractIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
